a092818796cc2b7e9822192e7d46689d0a1ab2a2
Trevor Forrey <trevor4e@gmail.com>
2018-02-23 17:52:01 -0700

    beginning of color picker page

--- web/subwayShibori_webApp/src/ColorPicker.vue
+++ web/subwayShibori_webApp/src/ColorPicker.vue
@@ -1,6 +1,13 @@
 <template>
-  <div id="home">
+  <div id="colorPicker">
     <h3>Color Picker Page</h3>
+    <div id='colors'>
+      <div id='color1'></div>
+    </div>
+    <button v-on:click='sendColor'>Update Color</button>
+    <!-- It would be a good idea to create a read characteristic of the current led color
+    so show what current vs new is on the page.
+    Note: I can bind the color of a div to the data of the last color picked -->
   </div>
 </template>
 
@@ -11,11 +18,14 @@ export default {
       colorService: '0x1234',
       colorWriteCharacteristic: '0x002',
       jacket: {},
-      colorWriteCharRef: 'nothing'
+      colorWriteCharRef: 'nothing',
+      newColor: 'blue'
     }
   },
   methods: {
-
+    sendColor: function() {
+      console.log('sending color to ble feather');
+    }
   }
 }
 </script>
@@ -28,6 +38,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#colors {
+  width: 50%;
+  margin: 0px auto;
+}
+
+#color1 {
+  width: 4em;
+  height: 4em;
+  background-color: blue;
 }
 
 h1, h2 {
